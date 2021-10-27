c550fbd8ffdf7f2aac4c3a9efc27d41b206cd98e
Alex Belyaev <test2@leoni.land>
2021-10-27 14:54:40 +0300

    Initial commit

---
+++ src/main/java/ru/sber/sberproject/SberProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.sberproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberProjectApplication.class, args);
+    }
+
+}
